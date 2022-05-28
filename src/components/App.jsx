1bf5430d55e4fedd1de32aa5c35ce7ae3cd3ef34
boaz <boaserem022@gmail.com>
2022-05-28 05:32:04 +0300

    third commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
+
+
+//Challenge:
+
+//2. Given that you can get code to be called every second
+
+
+
+//e.g. uncomment the code below to see Hey printed every second.
+// function sayHi() {
+//   console.log("Hey");
+// }
+// setInterval(sayHi, 1000);
 
 function App() {
+  // get the current time using:
+  let time = new Date().toLocaleTimeString().substring(0,7);
+  // console.log(time);
+
+  let [initialTime,setTime] = useState(time);
+  
+
+  function updateTime (){
+    let currentTime = new Date().toLocaleTimeString().substring(0,7);;
+    setTime(currentTime);
+  }
+  //getting the time in your <h1> to update every second?
+  //using the setInterval method.
+  setInterval(updateTime, 1000);
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      {/* //Show the latest time in the <h1> when the Get Time button
+      //is pressed. */}
+      <h1>{initialTime}</h1>
+      <button onClick = {updateTime}>Get Time</button>
     </div>
-  );
-}
+  )
+};
 
 export default App;
